2c9b952ecdbc7dcff5cd04b74693b894d78d3d07
DesiredName <wc3-enter@yandex.ru>
2025-04-26 18:03:35 +0200

    'update'

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const date_range_option = Object.freeze({
     L90D: 'l90d',
     LY: 'ly',
     LIFE: 'Lifetime',
+    Y2025: '2025',
+    Y2024: '2024',
 });
 
 const date_range = Object.freeze({
@@ -18,6 +20,15 @@ const date_range = Object.freeze({
     [date_range_option.L90D]: 'Last 90 days',
     [date_range_option.LY]: 'Last 365 days',
     [date_range_option.LIFE]: 'Lifetime',
+    [date_range_option.Y2025]: '2025',
+    [date_range_option.Y2024]: '2024',
+});
+
+const view_option = Object.freeze({
+    VIEWS: 'EXTERNAL_VIEWS-tab',
+    WATCH: 'EXTERNAL_WATCH_TIME-tab',
+    SUBS: 'SUBSCRIBERS_NET_CHANGE-tab',
+    REV: 'TOTAL_ESTIMATED_EARNINGS-tab',
 });
 
 const compute_date_range_DaysFormatted = (option) => {
@@ -28,11 +39,6 @@ const compute_date_range_DaysFormatted = (option) => {
     let prev = 0;
 
     switch (option) {
-        default:
-        case date_range_option.L7D:
-            prev = new Date(now - 6 * day);
-            break;
-
         case date_range_option.L28D:
             prev = new Date(now - 27 * day);
             break;
@@ -48,10 +54,45 @@ const compute_date_range_DaysFormatted = (option) => {
         case date_range_option.LIFE:
             prev = new Date(2023, 3, 19); // 19 Apr 2023
             break;
+
+        default:
+        case date_range_option.L7D:
+            prev = new Date(now - 6 * day);
+            break;
     }
 
     if (curr.getFullYear() === prev.getFullYear()) {
-        if (curr.getMonth() === prev.getMonth()) {
+        if (option === date_range_option.Y2025) {
+            const f1 = Intl.DateTimeFormat(undefined, {
+                day: '2-digit',
+                month: 'short',
+            });
+            const f2 = Intl.DateTimeFormat(undefined, {
+                day: '2-digit',
+                month: 'short',
+                year: 'numeric'
+            });
+
+            const month_prev = f1.format(new Date(curr.getFullYear(), 0));
+            const month_curr = f2.format(new Date(curr));
+
+            return month_prev + ' - ' + month_curr;
+        } else if (option === date_range_option.Y2024) {
+            const f1 = Intl.DateTimeFormat(undefined, {
+                day: '2-digit',
+                month: 'short',
+            });
+            const f2 = Intl.DateTimeFormat(undefined, {
+                day: '2-digit',
+                month: 'short',
+                year: 'numeric'
+            });
+
+            const month_prev = f1.format(new Date(option, 0));
+            const month_curr = f2.format(new Date(option, 12, 0));
+
+            return month_prev + ' - ' + month_curr;
+        } else if (curr.getMonth() === prev.getMonth()) {
             const f = Intl.DateTimeFormat(undefined, {
                 month: 'short'
             });
@@ -85,6 +126,9 @@ const state = reactive({
     date_range_option,
     date_range,
 
+    view_option,
+    selected_view_option: null,
+
     isDateSelectorVisible: false,
     selectedDateRange_DaysFormatted: compute_date_range_DaysFormatted(date_range_option.L7D),
     selectedDateRangeOption: date_range[date_range_option.L7D],
@@ -113,6 +157,12 @@ const state = reactive({
         this.hideDateSelector();
         this.selectedDateRangeOption = date_range[option];
         this.selectedDateRange_DaysFormatted = compute_date_range_DaysFormatted(option)
+    },
+
+    selectChartView(option) {
+        this.selected_view_option?.classList.remove('iron-selected')
+        this.selected_view_option = document.getElementById(option);
+        this.selected_view_option?.classList.add('iron-selected')
     }
 });
 
@@ -121,5 +171,7 @@ createApp({
     state,
     mounted() {
         state.hideDateSelector();
-    }
+        state.selectDateRangeId(date_range.l7d);
+        state.selectChartView(view_option.WATCH);
+    },
 }).mount();
