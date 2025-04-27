19955367d6056d221650ad7880d061891d8f7898
DesiredName <wc3-enter@yandex.ru>
2025-04-27 13:44:05 +0200

    'update'

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
 import { createApp, reactive } from 'https://unpkg.com/petite-vue?module'
+import {
+    addVerticalLine,
+    date_range,
+    date_range_option,
+    compute_date_range_DaysFormatted,
+    compute_live_viewers
+} from './utils.js';
 
 // elements
 const date_range_selector_toggler_el = document.querySelector("[data-id='date_range_selector_toggler']")
 const date_range_selector_el = document.querySelector("[data-id='date_range_selector']");
 const chart_1_placeholder = document.querySelector("[data-id='chart-1-placeholder']");
-
-const date_range_option = Object.freeze({
-    L7D: 'l7d',
-    L28D: 'l28d',
-    L90D: 'l90d',
-    LY: 'ly',
-    LIFE: 'Lifetime',
-    Y2025: '2025',
-    Y2024: '2024',
-});
-
-const date_range = Object.freeze({
-    [date_range_option.L7D]: 'Last 7 days',
-    [date_range_option.L28D]: 'Last 28 days',
-    [date_range_option.L90D]: 'Last 90 days',
-    [date_range_option.LY]: 'Last 365 days',
-    [date_range_option.LIFE]: 'Lifetime',
-    [date_range_option.Y2025]: '2025',
-    [date_range_option.Y2024]: '2024',
-});
+const chart_2_placeholder = document.querySelector("[data-id='chart-2-placeholder']");
 
 const view_option = Object.freeze({
     VIEWS: 'EXTERNAL_VIEWS-tab',
@@ -31,105 +19,6 @@ const view_option = Object.freeze({
     SUBS: 'SUBSCRIBERS_NET_CHANGE-tab',
     REV: 'TOTAL_ESTIMATED_EARNINGS-tab',
 });
-
-const compute_date_range_DaysFormatted = (option) => {
-    const day = 24 * 60 * 60 * 1000;
-    const now = Date.now() - day;
-
-    const curr = new Date(now);
-    let prev = 0;
-
-    switch (option) {
-        case date_range_option.L28D:
-            prev = new Date(now - 27 * day);
-            break;
-
-        case date_range_option.L90D:
-            prev = new Date(now - 89 * day);
-            break;
-
-        case date_range_option.LY:
-            prev = new Date(now - 364 * day);
-            break;
-
-        case date_range_option.LIFE:
-            prev = new Date(2023, 3, 19); // 19 Apr 2023
-            break;
-
-        default:
-        case date_range_option.L7D:
-            prev = new Date(now - 6 * day);
-            break;
-    }
-
-    if (curr.getFullYear() === prev.getFullYear()) {
-        if (option === date_range_option.Y2025) {
-            const f1 = Intl.DateTimeFormat('en', {
-                day: '2-digit',
-                month: 'short',
-            });
-            const f2 = Intl.DateTimeFormat('en', {
-                day: '2-digit',
-                month: 'short',
-                year: 'numeric'
-            });
-
-            const month_prev = f1.format(new Date(curr.getFullYear(), 0));
-            const month_curr = f2.format(new Date(curr));
-
-            return month_prev + ' - ' + month_curr;
-        } else if (option === date_range_option.Y2024) {
-            const f1 = Intl.DateTimeFormat('en', {
-                day: '2-digit',
-                month: 'short',
-            });
-            const f2 = Intl.DateTimeFormat('en', {
-                day: '2-digit',
-                month: 'short',
-                year: 'numeric'
-            });
-
-            const month_prev = f1.format(new Date(option, 0));
-            const month_curr = f2.format(new Date(option, 12, 0));
-
-            return month_prev + ' - ' + month_curr;
-        } else if (curr.getMonth() === prev.getMonth()) {
-            const f = Intl.DateTimeFormat('en', {
-                month: 'short'
-            });
-            const month = f.format(curr);
-
-            return month + ' ' + prev.getDate() + ' - ' + curr.getDate() + ', ' + curr.getFullYear();
-        } else {
-            const f = Intl.DateTimeFormat('en', {
-                day: '2-digit',
-                month: 'short'
-            });
-            const month_curr = f.format(curr);
-            const month_prev = f.format(prev);
-
-            return month_prev + ' - ' + month_curr + ', ' + curr.getFullYear();
-        }
-    } else {
-        const f = Intl.DateTimeFormat('en', {
-            day: '2-digit',
-            month: 'short',
-            year: 'numeric'
-        });
-        const month_curr = f.format(curr);
-        const month_prev = f.format(prev);
-
-        return month_prev + ' - ' + month_curr;
-    }
-}
-
-const compute_live_viewers = (num) => {
-    const f = new Intl.NumberFormat('en', {
-        style: 'decimal',
-    });
-
-    return f.format(num);
-}
 
 const state = reactive({
     date_range_option,
@@ -217,6 +106,7 @@ createApp({
         state.selectChartView(view_option.WATCH);
 
         SpinChart1();
+        SpinChart2();
     },
     uploadChart1Datas(e) {
         e.preventDefault();
@@ -225,47 +115,127 @@ createApp({
 }).mount();
 
 // CHARTS
-const addVerticalLine = (chart, renderer) => {
-    const xAxis = chart.xAxis[0];
-    const verticalLine = renderer
-        .path([['M', 0, 10], ['L', 0, chart.plotHeight + 10]])
-        .attr({
-            class: 'chart-vertical-line',
-            fill: '#7e7e7e',
-            stroke: '#282828',
-            'stroke-width': 1,
-        })
-        .add();
-
-    verticalLine.hide();
-
-    chart.container.addEventListener('pointermove', (event) => {
-        const nearest = chart.series?.[0]?.searchPoint?.(chart.pointer.normalize(event), true);
-        const idx = nearest?.key;
-
-        if (!!idx && idx >= xAxis.min && idx <= xAxis.max) {
-            const plotX = xAxis.toPixels(idx);
-
-            verticalLine.attr({
-                d: [['M', plotX, 10], ['L', plotX, chart.plotHeight + 10], ['L', plotX + 2, chart.plotHeight + 10], ['L', plotX + 2, 10], ['L', plotX, 10]],
-            });
-            verticalLine.show();
-        } else {
-            verticalLine.hide();
-        }
-    });
-
-    chart.container.addEventListener('mouseleave', () => {
-        verticalLine.hide();
-    });
-}
-
 function SpinChart1() {
     Highcharts.chart(chart_1_placeholder, {
         chart: {
             type: 'area',
             backgroundColor: 'transparent',
             height: 160,
+            spacingLeft: 20,
+            spacingRight: 20,
+            spacingBottom: 8,
+            events: {
+                load(event) {
+                    const target = event.target;
+                    const renderer = target.renderer;
+
+                    addVerticalLine(target, renderer);
+                },
+            }
+        },
+        credits: {
+            enabled: false,
+        },
+        title: false,
+        xAxis: {
+            type: 'datetime',
+            dateTimeLabelFormats: {
+                day: '%b %e, %Y'  // Format: "Jan 1, 2023"
+            },
+            labels: {
+                align: 'center',
+                y: 20,
+                overflow: 'justify',
+            },
+            padding: 30,
+            lineColor: '#9e9e9e',
+            tickWidth: 2,
+            tickLength: 6,
+            minPadding: 0,
+            maxPadding: 0,
+            startOnTick: true,
+            endOnTick: true,
+        },
+        yAxis: {
+            opposite: true,  // Places Y-axis on the right side
+            tickAmount: 4,
+            gridLineColor: '#323232',
+            gridLineWidth: 2,
+            title: false,
+            labels: {
+                format: '{value:.1f}',
+            },
+        },
+        legend: false,
+        series: [{
+            data: [
+                [Date.UTC(2023, 3, 1), 0.7],
+                [Date.UTC(2023, 3, 2), 0.8],
+                [Date.UTC(2023, 3, 3), 0.5],
+                [Date.UTC(2023, 3, 4), 0.6],
+                [Date.UTC(2023, 3, 5), 0.6],
+                [Date.UTC(2023, 3, 6), 0.5],
+                [Date.UTC(2023, 3, 7), 0.3],
+            ],
+            color: '#41b4d9',
+            lineColor: '#41b4d9',
+            lineWidth: 2,
+            fillColor: 'rgba(65, 180, 217, 0.1)',
+        }],
+        tooltip: {
+            hideDelay: 0,
+            outside: true,
+            className: 'chart-tooltip',
+            backgroundColor: 'rgb(40, 40, 40)',
+            borderColor: 'rgb(61, 61, 61)',
+            borderWidth: 1,
+            borderRadius: 12,
+            padding: 18,
+            shape: 'none',
+            formatter: function () {
+                return '<span class="tooltip-date">' + new Date(this.x).toLocaleDateString('en-US', {
+                    weekday: 'short',
+                    month: 'short',
+                    day: 'numeric',
+                    year: 'numeric'
+                }) + '</span><br/><br/><span class="tooltip-value">' + this.y + "</span>";
+            }
+        },
+        plotOptions: {
+            series: {
+                marker: {
+                    enabled: false,
+                    states: {
+                        hover: {
+                            enabled: true,
+                            radius: 5,
+                            lineColor: '#282828',
+                            lineWidth: 2,
+                        },
+                    },
+                },
+                states: {
+                    inactive: {
+                        opacity: 1
+                    },
+                    hover: {
+                        enabled: true,
+                        halo: { size: 4 },
+                        lineWidth: 2,
+                    },
+                },
+                pointPlacement: 'on'
+            }
+        },
+    });
+}
+
+function SpinChart2() {
+    Highcharts.chart(chart_2_placeholder, {
+        chart: {
+            type: 'bar',
+            backgroundColor: 'transparent',
+            height: 60,
             spacingLeft: 20,
             spacingRight: 20,
             spacingBottom: 8,
---
+++ utils.js
@@ -0,0 +1,156 @@
+// SUPPORT
+export const date_range_option = Object.freeze({
+    L7D: 'l7d',
+    L28D: 'l28d',
+    L90D: 'l90d',
+    LY: 'ly',
+    LIFE: 'Lifetime',
+    Y2025: '2025',
+    Y2024: '2024',
+});
+
+export const date_range = Object.freeze({
+    [date_range_option.L7D]: 'Last 7 days',
+    [date_range_option.L28D]: 'Last 28 days',
+    [date_range_option.L90D]: 'Last 90 days',
+    [date_range_option.LY]: 'Last 365 days',
+    [date_range_option.LIFE]: 'Lifetime',
+    [date_range_option.Y2025]: '2025',
+    [date_range_option.Y2024]: '2024',
+});
+
+export const compute_date_range_DaysFormatted = (option) => {
+    const day = 24 * 60 * 60 * 1000;
+    const now = Date.now() - day;
+
+    const curr = new Date(now);
+    let prev = 0;
+
+    switch (option) {
+        case date_range_option.L28D:
+            prev = new Date(now - 27 * day);
+            break;
+
+        case date_range_option.L90D:
+            prev = new Date(now - 89 * day);
+            break;
+
+        case date_range_option.LY:
+            prev = new Date(now - 364 * day);
+            break;
+
+        case date_range_option.LIFE:
+            prev = new Date(2023, 3, 19); // 19 Apr 2023
+            break;
+
+        default:
+        case date_range_option.L7D:
+            prev = new Date(now - 6 * day);
+            break;
+    }
+
+    if (curr.getFullYear() === prev.getFullYear()) {
+        if (option === date_range_option.Y2025) {
+            const f1 = Intl.DateTimeFormat('en', {
+                day: '2-digit',
+                month: 'short',
+            });
+            const f2 = Intl.DateTimeFormat('en', {
+                day: '2-digit',
+                month: 'short',
+                year: 'numeric'
+            });
+
+            const month_prev = f1.format(new Date(curr.getFullYear(), 0));
+            const month_curr = f2.format(new Date(curr));
+
+            return month_prev + ' - ' + month_curr;
+        } else if (option === date_range_option.Y2024) {
+            const f1 = Intl.DateTimeFormat('en', {
+                day: '2-digit',
+                month: 'short',
+            });
+            const f2 = Intl.DateTimeFormat('en', {
+                day: '2-digit',
+                month: 'short',
+                year: 'numeric'
+            });
+
+            const month_prev = f1.format(new Date(option, 0));
+            const month_curr = f2.format(new Date(option, 12, 0));
+
+            return month_prev + ' - ' + month_curr;
+        } else if (curr.getMonth() === prev.getMonth()) {
+            const f = Intl.DateTimeFormat('en', {
+                month: 'short'
+            });
+            const month = f.format(curr);
+
+            return month + ' ' + prev.getDate() + ' - ' + curr.getDate() + ', ' + curr.getFullYear();
+        } else {
+            const f = Intl.DateTimeFormat('en', {
+                day: '2-digit',
+                month: 'short'
+            });
+            const month_curr = f.format(curr);
+            const month_prev = f.format(prev);
+
+            return month_prev + ' - ' + month_curr + ', ' + curr.getFullYear();
+        }
+    } else {
+        const f = Intl.DateTimeFormat('en', {
+            day: '2-digit',
+            month: 'short',
+            year: 'numeric'
+        });
+        const month_curr = f.format(curr);
+        const month_prev = f.format(prev);
+
+        return month_prev + ' - ' + month_curr;
+    }
+}
+
+export const compute_live_viewers = (num) => {
+    const f = new Intl.NumberFormat('en', {
+        style: 'decimal',
+    });
+
+    return f.format(num);
+}
+
+
+// CHARTS
+export const addVerticalLine = (chart, renderer) => {
+    const xAxis = chart.xAxis[0];
+    const verticalLine = renderer
+        .path([['M', 0, 10], ['L', 0, chart.plotHeight + 10]])
+        .attr({
+            class: 'chart-vertical-line',
+            fill: '#7e7e7e',
+            stroke: '#282828',
+            'stroke-width': 1,
+        })
+        .add();
+
+    verticalLine.hide();
+
+    chart.container.addEventListener('pointermove', (event) => {
+        const nearest = chart.series?.[0]?.searchPoint?.(chart.pointer.normalize(event), true);
+        const idx = nearest?.key;
+
+        if (!!idx && idx >= xAxis.min && idx <= xAxis.max) {
+            const plotX = xAxis.toPixels(idx);
+
+            verticalLine.attr({
+                d: [['M', plotX, 10], ['L', plotX, chart.plotHeight + 10], ['L', plotX + 2, chart.plotHeight + 10], ['L', plotX + 2, 10], ['L', plotX, 10]],
+            });
+            verticalLine.show();
+        } else {
+            verticalLine.hide();
+        }
+    });
+
+    chart.container.addEventListener('mouseleave', () => {
+        verticalLine.hide();
+    });
+}
